d4063ef5998319140fbf421c7fee9242914ceb7c
Benaya Andrias <113487774+basedcodesBen@users.noreply.github.com>
2022-12-24 12:09:33 +0700

    Co-authored-by: Kevin Owen <kvinown@users.noreply.github.com> Co-authored-by: Josephine Alvina L <Jsphinlvina@users.noreply.github.com>

--- scriptindex.js
+++ scriptindex.js
@@ -1,23 +1,21 @@
-let inputNama = prompt("Enter your name here:")
-document.getElementById('nameInput').innerHTML = inputNama
+let inputNama = prompt("Enter your name here:");
+document.getElementById("nameInput").innerHTML = inputNama;
 
-const background = document.querySelector('html');
-const button = document.querySelector('.contrast__link');
-button.addEventListener('click', e => {
+const background = document.querySelector("html");
+const button = document.querySelector(".contrast__link");
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  if (background.classList.contains('bg1')) {
-    background.classList.remove('bg1');
-    background.classList.add('bg2');
+  if (background.classList.contains("bg1")) {
+    background.classList.remove("bg1");
+    background.classList.add("bg2");
+  } else if (background.classList.contains("bg2")) {
+    background.classList.remove("bg2");
+    background.classList.add("bg3");
+  } else if (background.classList.contains("bg3")) {
+    background.classList.remove("bg3");
+    background.classList.add("bg1");
   }
-  else if (background.classList.contains('bg2')) {
-    background.classList.remove('bg2');
-    background.classList.add('bg3');
-  }
-  else if (background.classList.contains('bg3')) {
-    background.classList.remove('bg3');
-    background.classList.add('bg1');
-  }
-//   else {
-//     background.classList.add('bg1');
-//   }
+  //   else {
+  //     background.classList.add('bg1');
+  //   }
 });
